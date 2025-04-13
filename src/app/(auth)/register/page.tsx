bb0537cdb95d897b333c4476925800ef12dfd795
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-12 23:52:01 -0500

    feat: enhance registration form with name input and validation

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,15 +2,26 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import Image from 'next/image'
+import { Input } from '@/components/Input'
 
 const Register = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
 
 
   return (
-    <div  className='flex w-full h-full justify-center ' >
-      <div className='w-1/3 flex flex-col justify-end'>
+    <div  className='flex w-full h-full justify-center gap-8 ' >
+      <div className='w-1/3 flex flex-col justify-end '>
         <h1 className="text-3xl">Register Now</h1>
+
+        <form>
+
+          <div>
+            <label htmlFor="name" className='text-sm'>Name</label>
+            <Input placeholder='Email' type='email' {...register("name", { required: true })} />
+            {errors.name && <span className='text-red-500 text-sm'>This field is required</span>}
+          </div>
+
+        </form>
 
       </div>
       <div className='relative w-1/3 min-h-full '>

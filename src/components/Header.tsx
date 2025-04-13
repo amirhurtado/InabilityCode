a5e16bb272df08ba395cce3b095e928c7edc2ab3
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-12 23:17:01 -0500

    style: update header layout for improved responsiveness and add background styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
     <div className="container">
-      <div className="relative h-20">
-        <Image
-          src="/logo.svg"
-          alt="logo"
-          fill
-          className="object-contain object-left"
-        />
+      <div className="flex justify-between items-center">
+        <div className="relative h-20  w-full">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            fill
+            className="object-contain object-left"
+          />
+        </div>
+        <ModeToggle />
       </div>
     </div>
   );

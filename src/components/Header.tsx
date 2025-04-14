3711b8489c71616334ece26d1b0e116a92b31b29
Amir Hurtado <a.hurtado@utp.edu.co>
2025-04-13 22:48:52 -0500

    feat: replace logo image with SVG component; improve theme-based color handling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import Image from "next/image";
 import { ModeToggle } from "./ModeToggle";
 import { DocumentData } from "firebase-admin/firestore";
 import { LogOut } from 'lucide-react';
 import { Button } from "./Button";
 import { logout } from "@/app/services/auth/actions";
 import Link from "next/link";
+import Logo from "./Logo";
 
 const Header = ({user} : DocumentData) => {
   return (
       <div className="flex justify-between items-center">
-        <Link href={user ? "/dashboard" : "/"} className="relative h-20  w-full">
-          <Image
-            src="/logo.svg"
-            alt="logo"
-            fill
-            className="object-contain object-left"
-          />
+        <Link href={user ? "/dashboard" : "/"} className="flex relative h-20  w-full">
+          <Logo />
         </Link>
         <div className="flex items-center gap-5">
           <ModeToggle />
